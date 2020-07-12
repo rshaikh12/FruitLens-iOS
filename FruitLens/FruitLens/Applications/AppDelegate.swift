4a510b4b465a20cd8c9ae581bd605c95cbe199f8
Roxana Shaikh <shaikh@cip.ifi.lmu.de>
2020-07-12 17:27:16 +0200

    Update AppDelegate.swift

--- FruitLens/FruitLens/Applications/AppDelegate.swift
+++ FruitLens/FruitLens/Applications/AppDelegate.swift
@@ -40,8 +40,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let container = NSPersistentContainer(name: "Model")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                // fatalError() causes the application to generate a crash log and terminate.
+                // Should be replaced 
 
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
@@ -56,7 +56,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             do {
                 try context.save()
             } catch {
-                // Replace this implementation with code to handle the error appropriately.
                 let nserror = error as NSError
                 fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
             }
