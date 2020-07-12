d4510060c64db9d0705715a1a76e5980f2d337bb
Christoph Weber <weberchr@cip.ifi.lmu.de>
2020-07-12 21:15:35 +0200

    fixed FatalError in Emulator

--- FruitLens/FruitLens/ViewControllers/Recognition/ViewController.swift
+++ FruitLens/FruitLens/ViewControllers/Recognition/ViewController.swift
@@ -27,7 +27,6 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
     private let videoDataOutputQueue = DispatchQueue(label: "VideoDataOutput", qos: .userInitiated, attributes: [], autoreleaseFrequency: .workItem)
     
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-        // to be implemented in the subclass
     }
     
     override func viewDidLoad() {
@@ -38,9 +37,7 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         setupAVCapture()
-        
         eatButton.layer.zPosition = 10
-        
     }
     
     override func didReceiveMemoryWarning() {
@@ -51,57 +48,64 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
     func setupAVCapture() {
         var deviceInput: AVCaptureDeviceInput!
         
+        
         // Select a video device, make an input
         let videoDevice = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: .video, position: .back).devices.first
         do {
-            deviceInput = try AVCaptureDeviceInput(device: videoDevice!)
+            if videoDevice == nil {
+                print("use a real phone!!!")
+                self.eatButton.isHidden = true
+            }
+            else {
+                deviceInput = try AVCaptureDeviceInput(device: videoDevice!)
+                
+                session.beginConfiguration()
+                session.sessionPreset = .vga640x480 // Model image size is smaller.
+                
+                // Add a video input
+                guard session.canAddInput(deviceInput) else {
+                    print("Could not add video device input to the session")
+                    session.commitConfiguration()
+                    return
+                }
+                session.addInput(deviceInput)
+                if session.canAddOutput(videoDataOutput) {
+                    session.addOutput(videoDataOutput)
+                    // Add a video data output
+                    videoDataOutput.alwaysDiscardsLateVideoFrames = true
+                    videoDataOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: Int(kCVPixelFormatType_420YpCbCr8BiPlanarFullRange)]
+                    videoDataOutput.setSampleBufferDelegate(self, queue: videoDataOutputQueue)
+                } else {
+                    print("Could not add video data output to the session")
+                    session.commitConfiguration()
+                    return
+                }
+                let captureConnection = videoDataOutput.connection(with: .video)
+                // Always process the frames
+                captureConnection?.isEnabled = true
+                do {
+                    try videoDevice!.lockForConfiguration()
+                    let dimensions = CMVideoFormatDescriptionGetDimensions((videoDevice?.activeFormat.formatDescription)!)
+                    bufferSize.width = CGFloat(dimensions.width)
+                    bufferSize.height = CGFloat(dimensions.height)
+                    videoDevice!.unlockForConfiguration()
+                } catch {
+                    print(error)
+                }
+                session.commitConfiguration()
+                previewLayer = AVCaptureVideoPreviewLayer(session: session)
+                previewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+                if let previewView = previewView {
+                    rootLayer = previewView.layer
+                    previewLayer.frame = rootLayer.bounds
+                    rootLayer.addSublayer(previewLayer)
+                }
+            }
+        } catch _ as NSError {
+            deviceInput = nil
         } catch {
-            print("Could not create video device input: \(error)")
-            return
+            fatalError()
         }
-        
-        session.beginConfiguration()
-        session.sessionPreset = .vga640x480 // Model image size is smaller.
-        
-        // Add a video input
-        guard session.canAddInput(deviceInput) else {
-            print("Could not add video device input to the session")
-            session.commitConfiguration()
-            return
-        }
-        session.addInput(deviceInput)
-        if session.canAddOutput(videoDataOutput) {
-            session.addOutput(videoDataOutput)
-            // Add a video data output
-            videoDataOutput.alwaysDiscardsLateVideoFrames = true
-            videoDataOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: Int(kCVPixelFormatType_420YpCbCr8BiPlanarFullRange)]
-            videoDataOutput.setSampleBufferDelegate(self, queue: videoDataOutputQueue)
-        } else {
-            print("Could not add video data output to the session")
-            session.commitConfiguration()
-            return
-        }
-        let captureConnection = videoDataOutput.connection(with: .video)
-        // Always process the frames
-        captureConnection?.isEnabled = true
-        do {
-            try videoDevice!.lockForConfiguration()
-            let dimensions = CMVideoFormatDescriptionGetDimensions((videoDevice?.activeFormat.formatDescription)!)
-            bufferSize.width = CGFloat(dimensions.width)
-            bufferSize.height = CGFloat(dimensions.height)
-            videoDevice!.unlockForConfiguration()
-        } catch {
-            print(error)
-        }
-        session.commitConfiguration()
-        previewLayer = AVCaptureVideoPreviewLayer(session: session)
-        previewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
-        if let previewView = previewView {
-            rootLayer = previewView.layer
-            previewLayer.frame = rootLayer.bounds
-            rootLayer.addSublayer(previewLayer)
-        }
-
     }
     
     func startCaptureSession() {
